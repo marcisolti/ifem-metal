654e2e6e8a45bcf22b357cbdc3aece7ea416e5f9
marcisolti <marci.solti.hu@gmail.com>
2022-05-22 18:25:42 +0200

    cleanup ShaderTypes, Shaders, basic perspective projection

--- Renderer/ShaderTypes.h
+++ Renderer/ShaderTypes.h
@@ -1,31 +1,23 @@
-/*
-See LICENSE folder for this sample’s licensing information.
+//
+//  ShaderTypes.h
+//  iFEM
+//
+//  Created by Marci Solti on 2022. 05. 21..
+//  Copyright © 2022. Apple. All rights reserved.
+//
 
-Abstract:
-Header containing types and enum constants shared between Metal shaders and C/ObjC source
-*/
-
-#ifndef ShaderTypes_h
-#define ShaderTypes_h
+#pragma once
 
 #include <simd/simd.h>
 
-// Buffer index values shared between shader and C code to ensure Metal shader buffer inputs
-// match Metal API buffer set calls.
 typedef enum VertexInputIndex
 {
-    VertexInputIndexVertices     = 0,
-    VertexInputIndexViewportSize = 1,
+    VertexInputIndexVertices = 0,
+    VertexInputIndexMVP      = 1,
 } VertexInputIndex;
 
-//  This structure defines the layout of vertices sent to the vertex
-//  shader. This header is shared between the .metal shader and C code, to guarantee that
-//  the layout of the vertex array in the C code matches the layout that the .metal
-//  vertex shader expects.
 typedef struct
 {
-    vector_float2 position;
-    vector_float4 color;
+    vector_float3 position;
+    vector_float3 normal;
 } Vertex;
-
-#endif /* ShaderTypes_h */
