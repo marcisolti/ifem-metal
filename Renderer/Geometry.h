a066ed6081ceb4684432de27f25dd64478e22058
marcisolti <marci.solti.hu@gmail.com>
2022-05-22 18:53:08 +0200

    make geometry a struct

--- Renderer/Geometry.h
+++ Renderer/Geometry.h
@@ -14,9 +14,8 @@
 
 template<typename VertexType,
          typename IndexType>
-class Geometry
+struct Geometry
 {
-public:
     Geometry() = default;
 
     Geometry(std::vector<VertexType> vertices, std::vector<IndexType> indices = {})
@@ -24,22 +23,6 @@ public:
     , indices{indices}
     { }
 
-    void PushVertex(VertexType i) { vertices.push_back(i); }
-    void PushIndex(IndexType v) { indices.push_back(v); }
-
-    void SetVertices(const std::vector<VertexType>& v) { vertices = v; }
-    void SetIndices(const std::vector<IndexType>& i) { indices = i; }
-
-    VertexType* VertexData() { return vertices.data(); }
-    VertexType* VertexData() const { return vertices.data(); }
-    IndexType* IndexData() { return indices.data(); }
-    IndexType* IndexData() const { return indices.data(); }
-
-    size_t VertexSize() const { return sizeof(VertexType) * vertices.size(); }
-    size_t IndexSize() const { return sizeof(IndexType) * indices.size(); }
-    size_t IndexCount() const { return indices.size(); }
-
-private:
     std::vector<VertexType> vertices;
     std::vector<IndexType> indices;
 };
